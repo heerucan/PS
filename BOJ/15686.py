2733f1089e459741ac3c260691685e42126814f2
heerucan <iwpow0307@gmail.com>
2023-01-10 23:07:25 +0900

    BOJ - 구현 15686 수정 #23

--- BOJ/15686.py
+++ BOJ/15686.py
@@ -1,14 +1,3 @@
-# n*n 도시, 빈칸0/치킨집2/집1
-# r,c (1,1)부터 시작 // (r1, c1)과 (r2, c2) 사이의 거리 = |r1-r2| + |c1-c2|
-# 치킨거리 : 집에서 가장 가까운 치킨집 사이의 거리 (모든 집이 다 가짐)
-# 도시의 치킨거리의 합 == 모든 집의 치킨 거리의 합
-
-# 가장 수익 많이 내는 치킨집 수 = 최대 M개
-
-# n : 도시 정보
-# m : 폐업시키지 않을 치킨집의 최대값
-# 출력값 : 도시의 치킨 거리의 최솟값
-
 from itertools import combinations
 
 n, m = map(int, input().split())
@@ -44,6 +33,7 @@ for chickenStore in combinations(chicken, m):
             yDistance = abs(h[1] - chickenStore[j][1])
             tempDistance = min(tempDistance, xDistance+yDistance) # 더 짧은 치킨거리를 찾기
         distance += tempDistance # 도시 전체 치킨 거리를 구하기 위해 각 집까지의 치킨거리를 구해서 더함
-
-result = min(result, distance) 
+    result = min(result, distance) 
 print(result)
+
+
