47621e99debd9b0cf0bec77b64a879528a8f1b30
heerucan <iwpow0307@gmail.com>
2023-03-08 21:41:10 +0900

    PRG - 구현 콜라즈 추측

---
+++ SwiftAlgorithm/SwiftAlgorithm/BFS&DFS/ㅡ먀ㅜ.swift
@@ -0,0 +1,8 @@
+//
+//  ㅡ먀ㅜ.swift
+//  SwiftAlgorithm
+//
+//  Created by heerucan on 2023/03/06.
+//
+
+import Foundation
---
+++ SwiftAlgorithm/SwiftAlgorithm/구현/콜라즈추측.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by heerucan on 2023/03/06.
+//
+
+import Foundation
+
+func calculate(_ value: Int) -> Int {
+    if value % 2 == 0 {
+        return value/2
+    } else {
+        return value*3+1
+    }
+}
+
+func solution(_ num: Int) -> Int {
+    var cnt = 0
+    var value = num
+    while value != 1 {
+        value = calculate(value)
+        cnt += 1
+        if cnt == 500 {
+            return -1
+        }
+    }
+    return cnt
+}
+
+print(solution(16))
