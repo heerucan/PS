37b5eb02b1cf7756a23ab4c0bbb788b8eb8d1113
heerucan <iwpow0307@gmail.com>
2023-03-10 00:48:49 +0900

    PRG - 구현 직사각형 별찍기

---
+++ SwiftAlgorithm/SwiftAlgorithm/구현/직사각형 별찍기.swift
@@ -0,0 +1,15 @@
+//
+//  직사각형 별찍기.swift
+//  SwiftAlgorithm
+//
+//  Created by heerucan on 2023/03/10.
+//
+
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+
+//print(String(repeating: String(repeating: "*", count: a) + "\n", count: b)
+//)
---
+++ SwiftAlgorithm/SwiftAlgorithm/구현/최대공약수와 최소공배수.swift
@@ -0,0 +1,8 @@
+//
+//  최대공약수와 최소공배수.swift
+//  SwiftAlgorithm
+//
+//  Created by heerucan on 2023/03/10.
+//
+
+import Foundation
