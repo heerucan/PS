98c3118654d106e0ea0a8700e24e162114991fd1
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-07-25 16:17:39 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,11 +1,7 @@
 def solution(food):
     answer = ''
-    water = False
-    if food[0] == 1:
-        water = True
-        
+    
     for i in range(1, len(food)):
-        if food[i] < 2: continue
         cnt = food[i]//2
         while cnt > 0:
             answer += str(i)
