b4f481e9038c6997fda6d95e05e4ce397b6d9c41
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-07-31 15:17:51 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    # numbers 각 원소를 문자열로 변경
+    # lambda를 사용해서 각 원소를 3번씩 반복시켜주고, 내림차순 정렬
+    # 1000 이하의 수라서 앞에서 3자리수끼리 비교하기 위함
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: x*3, reverse=True)
+    cnt = 0
+    
+    for i in numbers:
+        if i == '0':
+            cnt += 1
+    if len(numbers) == cnt:
+        return '0'
+    else:
+        return ''.join(numbers)
