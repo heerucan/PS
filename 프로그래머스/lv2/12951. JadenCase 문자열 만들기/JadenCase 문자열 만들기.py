16b7b4206cd8fc65ff55160f44eae28a5c743160
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-08-01 12:48:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,17 +1,8 @@
 # 문자열의 첫 문자를 대문자로 바꾸는 것 / 나머지는 소문자
 # 만약 첫 문자가 숫자면 그냥 냅둘 것
 def solution(s):
-    result = ""
-    is_new_word = True
-    
-    for char in s:
-        if char == " ":
-            is_new_word = True
-            result += char
-        elif is_new_word:
-            result += char.upper()
-            is_new_word = False
-        else:
-            result += char.lower()
-    
-    return result
+    test_list = s.split(" ")
+    result = []
+    for i in test_list :
+        result.append(i.capitalize())
+    return ' '.join(result)
