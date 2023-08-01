141233311b8aaef97e42019afb403b93ce43fc63
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-08-01 20:19:11 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,12 +1,8 @@
 # n보다 큰 자연수
 # n과, 큰 수는 2진수로 변환 시 1의 갯수가 같음
 def solution(n):
-    answer = 0
-    binN = bin(n)[2:]
-    oneCnt = binN.count('1')
+    oneCnt = bin(n)[2:].count('1')
     for i in range(n+1, 1000000):
         if bin(i)[2:].count('1') == oneCnt:
-            answer = i
-            break
+            return i
         
-    return answer
