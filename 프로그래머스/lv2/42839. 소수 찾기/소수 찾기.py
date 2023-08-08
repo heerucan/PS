c15093a58545abdef3290a26ecaf10ddeeaaf0ff
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-08-09 00:50:15 +0900

    [level 2] Title: 소수 찾기, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,26 +1,29 @@
 from itertools import permutations
 # 순서 상관있는 순열
+# 에라토스테네스의 체 비슷한 문제
+# 항상! 이런류의 문제는 is_prime 판별 메소드를 만들어서 풀 것!
+
+def is_prime(num):
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0: 
+            return False
+    return True
 
 def solution(numbers):
     answer = 0
     temp = []
-    a = []
+    arr = []
     for i in range(1, len(numbers)+1):
-        a = list(permutations(numbers, i))
-        for i in a:
-            newNum = int(''.join(i))
-            if newNum > 1 and newNum not in temp:
-                if newNum == 2:
-                    temp.append(newNum)
-                elif newNum % 2 != 0:
-                    temp.append(newNum)
-                
+        arr += list(permutations(numbers, i))
+    
+    noDuplicateArr = list(set(arr))
+    for i in noDuplicateArr:
+        newNum = int(''.join(i))
+        if newNum not in temp and newNum > 1:
+            temp.append(newNum)
+     
     for i in temp:
-        cnt = 0
-        for j in range(1, i+1):
-            if i % j == 0:
-                cnt += 1
-        if cnt == 2:
+        if is_prime(i):
             answer += 1
             
     return answer
