efeb09b709849e025c91ced3365f4b5daebf4864
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-08-10 00:04:40 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -4,12 +4,9 @@ def solution(citations):
     
     for h in range(len(citations)+1):
         big = 0
-        small = 0
         for j in citations:
             if j >= h: #기준값보다 인용횟수가 큰 경우, 즉-> h번 이상 인용된 논문의 개수
                 big += 1
-            else: #기준값보다 인용횟수가 작은 경우, 즉-> 나머지 논문의 개수
-                small += 1
-        if big >= h and small <= h:
+        if big >= h:
             answer = h
     return answer
