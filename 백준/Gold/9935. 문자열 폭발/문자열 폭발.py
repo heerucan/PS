c26d092afbbba6a7d3bd66e2caafe34468ced6ce
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-08-13 14:28:20 +0900

    [Gold IV] Title: 문자열 폭발, Time: 640 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-strings = input().rstrip()
+s = input().rstrip()
 bomb = input().rstrip()
-
-# 스택을 만들어서 bomb 문자 길이가 될 때까지 순회하며 넣다가, 만약 스택의 문자가 bomb과 같은 경우 
-# 스택에서 bomb 문자 길이만큼 pop해준다.
+bomb_len = len(bomb)
+# 스택을 사용해서 문자열을 다 넣어줘, 그리고 bomb 길이만큼 스택의 끝부분을 잘라서 bomb과 같으면 스택에서 제거해!
 
 stack = []
-bomb_len = len(bomb)
-
-for i in range(len(strings)):
-    stack.append(strings[i])  
-    if ''.join(stack[-bomb_len:]) == bomb:
-        for _ in range(bomb_len):
+for i in s:
+    stack.append(i)
+    if stack[-bomb_len:] == list(bomb):
+        for i in range(bomb_len):
             stack.pop()
 
-if stack:
-    print(''.join(stack))
+if ''.join(stack) == '':
+    print('FRULA')
 else:
-    print("FRULA")
+    print(''.join(stack))
