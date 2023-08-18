9821bc0027958e9d5804fb401945d4492b3ca06e
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-08-18 13:06:05 +0900

    [Silver II] Title: 섬의 개수, Time: 68 ms, Memory: 32740 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,73 +1,42 @@
-# 섬의 개수 세는 프로그램
-# 정사각형과 가로/세로/대각선 연결된 사각형은 걸어갈 수 있는 사각형
-# 지도의 너비 w, 높이 h
-# 1은 땅, 0은 바다
-
-# DFS
-
+# dfs로 그래프 개수 세기
 import sys
 sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
 
+def dfs(graph, visited, x, y):
+    # 대각선까지 추가
+    dx = [-1,1,0,0,-1,-1,1,1]
+    dy = [0,0,-1,1,-1,1,-1,1]
 
-
-def solution(w, h, graph):
-
-    def dfs(x, y):
-        #방문처리를 해줘야 함
-        graph[x][y] = 0 # 방문처리를 0으로 해줘야 함
-
-        # 상
-        if x-1 >= 0 and x-1 < h:
-            if graph[x-1][y] == 1:
-                dfs(x-1, y)
-        # 하
-        if x+1 >= 0 and x+1 < h:
-            if graph[x+1][y] == 1:
-                dfs(x+1, y)
-        # 좌
-        if y-1 >= 0 and y-1 < w:
-            if graph[x][y-1] == 1:
-                dfs(x, y-1)
-        # 우
-        if y+1 >= 0 and y+1 < w:
-            if graph[x][y+1] == 1:
-                dfs(x, y+1)
-
-        # 대각선
-        if x-1 >= 0 and x-1 < h and y-1 >= 0 and y-1 < w:
-            if graph[x-1][y-1] == 1:
-                dfs(x-1, y-1)
-
-        if x-1 >= 0 and x-1 < h and y+1 >= 0 and y+1 < w:
-            if graph[x-1][y+1] == 1:
-                dfs(x-1, y+1)
-
-        if x+1 >= 0 and x+1 < h and y-1 >= 0 and y-1 < w:
-            if graph[x+1][y-1] == 1:
-                dfs(x+1, y-1)
-
-        if x+1 >= 0 and x+1 < h and y+1 >= 0 and y+1 < w:
-            if graph[x+1][y+1] == 1:
-                dfs(x+1, y+1)
-        
-    result = 0
-    for i in range(h):
-        for j in range(w):
-            if graph[i][j] == 0:
-                continue
-            else:
-                dfs(i,j)
-                result+=1
+    visited[x][y] = True
     
-    print(result)
-    return result
+    for i in range(8):
+        xx = dx[i] + x
+        yy = dy[i] + y
+
+        if 0 > xx or xx > h-1 or 0 > yy or yy > w-1:
+            continue
+
+        if 0 <= xx <= h-1 and 0 <= yy <= w-1:
+            if not visited[xx][yy] and graph[xx][yy] == 1:
+                dfs(graph, visited, xx, yy)
+                visited[xx][yy] = True
 
 while True:
     w, h = map(int, input().split())
-    graph = []
-    for _ in range(h):
-        graph.append(list(map(int, input().split())))
     if w == 0 and h == 0:
         break
-    else:
-        solution(w, h, graph)
+    graph = []
+    visited = [[False for _ in range(w)] for _ in range(h)]
+    cnt = 0
+
+    for _ in range(h):
+        graph.append(list(map(int, input().rstrip().split())))
+
+    for i in range(h):
+        for j in range(w):
+            if not visited[i][j]:
+                if graph[i][j] == 1:
+                    dfs(graph, visited, i,j)
+                    cnt += 1
+    print(cnt)
