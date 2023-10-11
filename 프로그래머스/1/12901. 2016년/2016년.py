f05b716c3f492fd29751881f344bab1167e1d06c
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-10-11 13:11:42 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,20 +1,15 @@
 # 2016/1/1 금요일 // 윤년
 def solution(a, b):
-    answer = ''
+    weeks = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
     thirty = [1,3,5,7,8,10,12]
-    feb = 2
-    
     days = b
     
-    for i in range(1,a):
+    for i in range(1, a):
         if i in thirty:
             days += 31
-        elif i == feb:
+        elif i == 2:
             days += 29
         else:
             days += 30
             
-    weeks = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
     return weeks[days%7]
-    
-    # return answer
