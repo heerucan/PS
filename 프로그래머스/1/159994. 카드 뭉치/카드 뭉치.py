ba061cb40c93b345f2c4ad301ae046a1396572db
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-10-12 12:15:58 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -2,12 +2,11 @@
 
 def solution(cards1, cards2, goal):
     answer = ''
-    sentence = []
     for g in goal:
         if cards1 and g == cards1[0]:
-            sentence.append(cards1.pop(0))
+            cards1.pop(0)
         elif cards2 and g == cards2[0]:
-            sentence.append(cards2.pop(0))
+            cards2.pop(0)
         else:
             return "No"
     return "Yes"         
