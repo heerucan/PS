f6d46c4993277ec20afd4ccb763c466b17d4cf56
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-10-27 00:25:37 +0900

    [level 2] Title: 미로 탈출, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.07 ms
+메모리: 10.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 5일 0:20:34
+2023년 10월 5일 0:25:36
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -6,9 +6,14 @@ def solution(maps):
     n,m = len(maps), len(maps[0])
     visited = [[0 for _ in range(m)] for _ in range(n)]
     dx,dy = [-1,1,0,0], [0,0,-1,1]
-    temp = []
+    temp = {'S':(),'L':(),'E':()}
     
-    def bfs(xy,endX,endY):
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] in ('S', 'L', 'E'):
+                temp[maps[i][j]] = (i,j)
+    
+    def bfs(xy,endXY):
         visited = [[0 for _ in range(m)] for _ in range(n)]
         queue = deque()
         queue.append(xy)
@@ -27,28 +32,12 @@ def solution(maps):
                     queue.append((xx,yy))
                     visited[xx][yy] = visited[x][y]+1
                 
-        return visited[endX][endY]
+        return visited[endXY[0]][endXY[1]]
     
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 'S':
-                temp.append((i,j))
-                
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 'L':
-                temp.append((i,j))
-                
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j] == 'E':
-                temp.append((i,j))
-    
-    start = bfs(temp[0],temp[1][0],temp[1][1])
-    end = bfs(temp[1],temp[2][0],temp[2][1])
+    start = bfs(temp['S'],temp['L'])
+    end = bfs(temp['L'],temp['E'])
     
     if start == 0 or end == 0:
         return -1
     else:
         return start+end
-        
