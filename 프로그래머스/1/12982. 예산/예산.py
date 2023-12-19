ce156cb0081f8749cf4df37130c10ddecc318f8e
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2023-12-19 22:40:25 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,22 @@
+# 부서별로 신청한 금액이 들어있는 배열 d
+# 예산 budget
+# 최대 몇 개의 부서에 지원할 수 있는지 반환
+
+# 가장 신청 금액이 적은 부서부터 지원
+
+def solution(d, budget):
+    answer = 0
+    
+    # d 배열을 오름차순 정렬
+    # d를 돌면서 budget에서 하나씩 빼다가 budget이 0보다 작아지는 순간 stop
+    d.sort()
+    
+    for i in d:
+        budget -= i
+        if budget >= 0:
+            answer += 1
+        else:
+            break
+        
+    
+    return answer
