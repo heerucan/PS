df894c6fc3ce872834919838cc03bb2b682639b3
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2024-01-18 11:24:07 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16164 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -2,12 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
@@ -15,9 +11,5 @@ public class Main {
 		for (int i=1; i<10; i++) {
 			System.out.println(n+" * "+i+" = "+n*i);
 		}
-
-		bw.flush();
-		bw.close();
 	}
-
 }
