e27803bab0a365586be1b94ff0bd9063c8bcca40
Ruhee Kim <63235947+heerucan@users.noreply.github.com>
2024-01-28 23:52:12 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 140 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,40 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		/** 별찍기1 */
+//		for (int i=1; i<n+1; i++) {
+//			String star = "*";
+//			for (int k=0; k<i; k++) {
+//				System.out.print(star);
+//			}
+//			System.out.println();
+//		}
+		
+		/** repeat 사용 */
+//		for (int i=1; i<n+1; i++) {
+//			System.out.println("*".repeat(i));
+//		}
+		
+		/** 별찍기2 */
+//		for (int i=1; i<n+1; i++) {
+//			System.out.println(" ".repeat(n-i)+"*".repeat(i));
+//		}
+		
+		/** 별찍기3 */
+//		for (int i=n; i>=1; i--) {
+//			System.out.println("*".repeat(i));
+//		}
+		
+		/** 별찍기4 */
+		for (int i=n; i>=1; i--) {
+			System.out.println(" ".repeat(n-i)+"*".repeat(i));
+		}
+	}
+}
